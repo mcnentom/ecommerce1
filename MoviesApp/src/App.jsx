4ea983b5c561bc1f62a1b08ed60c2bf79e3fadbe
Stanleynjoroge <ngangastanley903@gmail.com>
2024-02-01 18:05:17 +0300

    my new app

---
+++ MoviesApp/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+import './App.css'
+import FetchComponent from './components/FetchComponent'
+import SearchBar from './components/SearchBar'
+
+function App() {
+
+
+  return (
+    <>
+      <SearchBar/>
+     <FetchComponent />
+   
+    </>
+  )
+}
+
+export default App
